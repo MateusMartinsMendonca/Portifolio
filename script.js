2a721b9b70f0ea8317bf9afee6c9b80f64e908d0
Mateus Martins <mateusmartinsm.31@gmail.com>
2022-08-11 21:43:49 -0300

    Mudança de rumo

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-function abrir_modal(projeto){
+function abrir_e_fechar_modal(projeto){
     let modal = document.querySelector(".modal-container" + projeto)
 
-  
-        modal.style.display = "block"
+    modal.style.display = "block"
    
 
     addEventListener("click" , (e) =>{
         console.log(e.target.className)
+        if(e.target.className == "modal-container" + projeto){
+            modal.style.display = "none"
+        }
     })
-    
-    
 }
+
+function fechar_modal(projeto){
+    let modal = document.querySelector(".modal-container" + projeto)
+    modal.style.display = "none"
+}
+
+
+    
